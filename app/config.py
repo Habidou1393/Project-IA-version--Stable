b09f3147ce85699d80eb943d9aac6d6b31b40e5c
Haytham Ladhari <Haytham.ladhari@protonmail.com>
2025-06-03 16:31:29 +0200

    .

--- app/config.py
+++ app/config.py
@@ -1,4 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CX = os.getenv("GOOGLE_CX")
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
 # Nom du fichier JSON qui stockera la mémoire ou les données du chatbot
+
 DATA_FILE = "MémoireDuChatbot.json"
 
 TAILLE_MAX = 1000  # Taille maximale de la mémoire, nombre d'entrées à conserver
@@ -6,17 +16,8 @@ TAILLE_MAX = 1000  # Taille maximale de la mémoire, nombre d'entrées à conser
 # Mot-clé déclencheur pour lancer une recherche sur Wikipédia dans les requêtes utilisateur
 WIKI_TRIGGER = "recherche sur wikipedia"
 
-# Clé API Google (à ne pas exposer publiquement pour des raisons de sécurité)
-GOOGLE_API_KEY = "AIzaSyCq-UUQBkDw1BpdEtlA0RHYtAZQ9mXq2O0"
-
 # Mot-clé déclencheur pour lancer une recherche sur Google dans les requêtes utilisateur
 GOOGLE_TRIGGER = "recherche sur google"
 
-# Identifiant du moteur de recherche personnalisé Google (Custom Search Engine ID)
-GOOGLE_CX = "6098340421bc7410b"
-
-MISTRAL_API_KEY = "MN7d3TwFQFGN4JQmQakW8qYCIq9UKOkX" 
-
+# Mot-clé déclencheur pour résoudre des expressions mathématiques dans les requêtes utilisateur
 MATH_TRIGGER = "maths : "
-
-
